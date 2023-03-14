3e118e0c9db075401d0e3d08287ea103933d0b3d
Aisha Hussein <aishasalaam07@gmail.com>
2023-03-14 04:30:26 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Aisha Hussein";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const firstInitial = currentUser[0];
+ const restOfName = currentUser.slice(1);
+ let shortGreeting = `Welcome, ${firstInitial}!`;
